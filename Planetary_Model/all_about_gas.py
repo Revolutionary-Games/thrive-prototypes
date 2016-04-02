8eed7e50423152bf15841425f3cdf61f97f53ffd
TJWhale <tjwhale@hotmail.com>
2016-04-02 10:31:09 +0100

    Added Maxwell-Boltzmann

--- Planetary_Model/all_about_gas.py
+++ Planetary_Model/all_about_gas.py
@@ -2,11 +2,16 @@ import math
 import random
 
 radius_of_earth = 6.37e6 #meters
+mass_of_mercury = 3.285e23 #kg
 mass_of_earth = 5.972e24 #kg
+mass_of_mars = 6.39e23 #kg
+mass_of_jupiter = 1.898e27 #kg
 gravity_constant = 6.674e-11 # N m^2/kg^2
 gas_constant = 8.3144598 # J mol^-1 K^-1
 molecular_mass_nitrogen = 0.028 # kg mol^-1
 molecular_mass_helium = 0.004 # kg mol^-1
+Boltzmann = 1.3803e-23 # J K^-1
+Avagadro =  6.0221e23
 
 #assuming all rocky planets are the same density as earth radius and mass are directly related
 def compute_radius_from_mass(mass):
@@ -35,3 +40,25 @@ print "Gas Velocity of Nitrogen = ", compute_gas_velocity(310, molecular_mass_ni
 print "Gas Velocity of Helium = ", compute_gas_velocity(310, molecular_mass_helium), " should be 1390."
 print "Surface Velocity = ", compute_surface_velocity(mass_of_earth, 24*60*60), " should be 460 ms^-1"
 
+def compute_maxwell_boltzmann(mass, temperature, velocity):
+	return (math.sqrt((float(mass)/float(2*math.pi*Boltzmann*temperature))**3)*
+			4*math.pi*(velocity**2)*math.exp(-float(mass*(velocity**2))/float(2*Boltzmann*temperature)))
+
+
+print "Maxwell Boltzmann at 310K"
+print "Mercury, Nitrogen:", compute_maxwell_boltzmann(molecular_mass_nitrogen/Avagadro, 
+				310, compute_escape_velocity(mass_of_mercury))
+print "Mercury, Helium:", compute_maxwell_boltzmann(molecular_mass_helium/Avagadro, 
+				310, compute_escape_velocity(mass_of_mercury))
+print "Earth, Nitrogen:", compute_maxwell_boltzmann(molecular_mass_nitrogen/Avagadro, 
+				310, compute_escape_velocity(mass_of_earth))
+print "Earth, Helium:", compute_maxwell_boltzmann(molecular_mass_helium/Avagadro, 
+				310, compute_escape_velocity(mass_of_earth))
+print "Mars, Nitrogen:", compute_maxwell_boltzmann(molecular_mass_nitrogen/Avagadro, 
+				310, compute_escape_velocity(mass_of_mars))
+print "Mars, Helium:", compute_maxwell_boltzmann(molecular_mass_helium/Avagadro, 
+				310, compute_escape_velocity(mass_of_mars))
+print "Jupiter, Nitrogen:", compute_maxwell_boltzmann(molecular_mass_nitrogen/Avagadro, 
+				310, compute_escape_velocity(mass_of_jupiter))
+print "Jupiter, Helium:", compute_maxwell_boltzmann(molecular_mass_helium/Avagadro, 
+				310, compute_escape_velocity(mass_of_jupiter))
