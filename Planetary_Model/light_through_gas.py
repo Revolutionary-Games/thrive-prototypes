2bd9f1928a8c6bf484819ec574aa3c0303dfcfa2
TJWhale <tjwhale@hotmail.com>
2016-04-15 18:40:54 +0100

    compute fudge factors

--- Planetary_Model/light_through_gas.py
+++ Planetary_Model/light_through_gas.py
@@ -3,6 +3,8 @@
 import math
 
 Avagadro = 6.022e23
+molecular_mass_carbon_dioxide = 0.044 # kg mol^-1
+molecular_mass_oxygen = 0.032 # kg mol^-1
 molecular_mass_nitrogen = 0.028 # kg mol^-1
 molecular_mass_water = 0.018 # kg mol^-1
 molecular_mass_methane = 0.016 # kg mol^-1
@@ -32,12 +34,62 @@ def atoms_of_gas_in_1sqm(radius, mass_of_gas, molecular_mass):
 
 print "Atoms of gas in a 1sqm column of air, ",
 print atoms_of_gas_in_1sqm(radius_of_earth, mass_of_earth_atmosphere, molecular_mass_nitrogen)
+print " "
 
-def attenuation_parameter(radius, mass_of_gas, molecular_mass, molecular_area):
+def attenuation_parameter(fudge_factor, radius, mass_of_gas, molecular_mass, molecular_area):
 	number_of_atoms = atoms_of_gas_in_1sqm(radius, mass_of_gas, molecular_mass)
-	exponent = -number_of_atoms*molecular_area
-	print "Light getting through = exp(", exponent, ") = ",
+	exponent = -fudge_factor*number_of_atoms*molecular_area
 	return math.exp(exponent)
 
-print attenuation_parameter(radius_of_earth, 0.7*mass_of_earth_atmosphere, 
+def compute_fudge_factor(desired_transmittance, radius, mass_of_gas, molecular_mass, molecular_area):
+	number_of_atoms = atoms_of_gas_in_1sqm(radius, mass_of_gas, molecular_mass)
+	exponent = -number_of_atoms*molecular_area
+	print "Current exponent = ", exponent
+	print "Desired exponent = ", math.log(desired_transmittance)
+	fudge_factor = float(math.log(desired_transmittance))/exponent
+	print "Fudge Factor = ", fudge_factor
+	return fudge_factor
+
+print "Compute fudge for nitrogen to get 0.5 transmittance"
+
+fudge_nitrogen = compute_fudge_factor(0.5, radius_of_earth, mass_of_earth_atmosphere*0.78, 
 			molecular_mass_nitrogen, Nitrogen_diameter**2)
+
+attenuation_nitrogen = attenuation_parameter(fudge_nitrogen, radius_of_earth, mass_of_earth_atmosphere*0.78, 
+			molecular_mass_nitrogen, Nitrogen_diameter**2)
+
+print "Nitrogen transmittance = ", attenuation_nitrogen
+print " "
+
+print "Compute fudge for oxygen to get 0.5 transmittance"
+
+fudge_oxygen = compute_fudge_factor(0.5, radius_of_earth, mass_of_earth_atmosphere*0.2, 
+			molecular_mass_oxygen, Oxygen_diameter**2)
+
+attenuation_oxygen = attenuation_parameter(fudge_oxygen, radius_of_earth, mass_of_earth_atmosphere*0.2, 
+			molecular_mass_oxygen, Oxygen_diameter**2)
+
+print "Oxygen transmittance = ", attenuation_oxygen
+print " "
+
+print "Compute fudge for water to get 0.5 transmittance"
+
+fudge_water = compute_fudge_factor(0.5, radius_of_earth, mass_of_earth_atmosphere*0.04, 
+			molecular_mass_water, Water_diameter**2)
+
+attenuation_water = attenuation_parameter(fudge_water, radius_of_earth, mass_of_earth_atmosphere*0.04, 
+			molecular_mass_water, Water_diameter**2)
+
+print "Water transmittance = ", attenuation_water
+print " "
+
+print "Compute fudge for carbon dioxide to get 0.5 transmittance"
+
+fudge_carbon_dioxide = compute_fudge_factor(0.5, radius_of_earth, mass_of_earth_atmosphere*0.04, 
+			molecular_mass_carbon_dioxide, Carbon_dioxide_diameter**2)
+
+attenuation_carbon_dioxide = attenuation_parameter(fudge_carbon_dioxide, radius_of_earth, mass_of_earth_atmosphere*0.04, 
+			molecular_mass_carbon_dioxide, Carbon_dioxide_diameter**2)
+
+print "Carbon dioxide transmittance = ", attenuation_carbon_dioxide
+print " "
