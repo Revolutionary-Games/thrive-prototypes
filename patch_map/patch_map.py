bec2460dcb7d8322db5876615341a12f5fb892d4
TjWhale <tjwhale@hotmail.com>
2018-12-19 11:57:28 +0000

    added better depth calculations and sunlight calcs

--- patch_map/patch_map.py
+++ patch_map/patch_map.py
@@ -13,39 +13,56 @@ screen.fill(background_colour)
 pygame.display.set_caption('Patch Map')
 pygame.font.init()
 
-myfont = pygame.font.SysFont("monospace", 20)
+myfont = pygame.font.SysFont("monospace", 20, bold = True)
 
 clock = pygame.time.Clock()
 
 def distance(a,b):
 	return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
-def get_type(pos):
+#given a screen position and a total depth of the ocean return the type of patch
+def get_type(pos, ocean_depth):
 	if pos[1] < 0.25*height:
 		if pos[0] < 0.33*width:
 			return "Ice Shelf"
 		return random.choice(["Tidepool", "Estuary"])
-	if pos[1] < 0.4*height:
+	patch_depth = ocean_depth*(pos[1] - height*0.25)/(height*0.75)
+	#epipelagic zone
+	if patch_depth < 200:
 		return random.choice(["Ocean Surface", "Coastal Shelf"])
-	if pos[1] < 0.6*height:
-		return random.choice(["Mid Ocean", "Mid Ocean", "Cave"])
-	if pos[1] < 0.8*height:
-		return random.choice(["Deep Ocean", "Hydrothermal"])
-	return "Ocean Floor"
+	#mesopelagic zone
+	if patch_depth < 700:
+		return random.choice(["Mid Ocean", "Mid Ocean", "Mid Ocean", "Cave"])
+	#bathypelagic zone
+	if patch_depth > 700 and pos[1] < 0.8*height:
+		return random.choice(["Deep Ocean", "Deep Ocean", "Deep Ocean", "Cave"])
+	#ocean floor
+	return random.choice(["Ocean Floor", "Ocean Floor", "Hydrothermal"])
 
 patch_names = ["Hydrothermal", "Ocean Floor", "Deep Ocean", "Mid Ocean", "Ocean Surface", "Coastal Shelf", "Tidepool", "Cave", "Ice Shelf", "Estuary"]
 
 patches = []
-min_seperation = 100 #pixels between patches
+min_seperation = 75 #pixels between patches
+max_number_of_caves = 4 #what is the max number of caves allowed?
+#note: photosynthesis is linear up to 100w/m^2 and then stops increasing. 
 
 class patch:
-	def __init__(self, pos, patch_type):
+	def __init__(self, pos, patch_type, ocean_depth, solar_strike):
+		#screen position
 		self.pos = pos
-		self.depth = int(3600*(pos[1] - 0.25*height)/(0.75*height))
+		#work out depth of the patch
+		self.depth = int(ocean_depth*(pos[1] - height*0.25)/(height*0.75))
 		if pos[1] < 0.25*height:
-			self.depth = int(50*(pos[1] - 0.25*height)/(0.25*height))
+			self.depth = int(15*(pos[1] - 0.25*height)/(0.25*height))
+		#work out how much light hits the patch
+		self.sunlight = min(solar_strike, math.exp(-0.023*self.depth)*solar_strike)
+		if patch_type == "Cave":
+			self.sunlight = 0
+		#what type of patch are you? from patch_names
 		self.patch_type = patch_type
+		#are you the patch where the player first starts?
 		self.starter_patch = False
+		#what other patches are you connected to?
 		self.neighbours = []
 
 		if patch_type == "Hydrothermal":
@@ -71,31 +88,38 @@ class patch:
 
 
 def reset():
+	#total ocean depth
+	ocean_depth = random.randint(1000,5000)
 	global patches
 	patches = []
 	needed_patches = patch_names[:]
 	#build patch list
 	#while there is room for another patch
+	cave_counter = 0
+	#amount of solar radiation strking the planet
+	solar_strike = random.randint(300,3000) #watts per m^2
 	while 1:
 		made_patch = False
 		patch_type = False
 		counter = 0
+		
 		#try to add a patch
 		while 1:
 			#choose random position
 			pos = [random.randint(10,width-10), random.randint(10,height-10)]
 			#find out what kind of patch the new one is
-			patch_type = get_type(pos)
+			patch_type = get_type(pos, ocean_depth)
 			#check if you need that patch to have one of each
 			need_patch = True
-			if len(needed_patches) > 0 and patch_type not in needed_patches:
+			if (len(needed_patches) > 0 and patch_type not in needed_patches) or (patch_type == "Cave" and cave_counter >= max_number_of_caves):
 				need_patch = False
 			#check if the position is too close to another
 			too_close = False
 			for p in patches:
 				if distance(p.pos,pos) < min_seperation:
 					too_close = True
-			if too_close == False:
+			#if the patch is not too close to another and is needed then make it 
+			if too_close == False and need_patch == True:
 				made_patch = True
 				break
 			#fail after 1000 attempts
@@ -105,13 +129,15 @@ def reset():
 		#if you failed to add a new patch you are done
 		if made_patch == False:
 			break
-		patches.append(patch(pos, patch_type))
+		patches.append(patch(pos, patch_type, ocean_depth, solar_strike))
 		if patch_type in needed_patches:
 			needed_patches.remove(patch_type)
+		if patch_type == "Cave":
+			cave_counter += 1
 
 	#work out where the starter patch is
 	best_dist = 10*width
-	best_patch = False
+	best_patch = patches[0]
 	for p in patches:
 		dist = abs(width/2 - p.pos[0])
 		if p.patch_type == "Hydrothermal" and dist < best_dist:
@@ -121,6 +147,7 @@ def reset():
 	print("Done")
 
 reset()
+
 
 running = True
 while running:
@@ -136,6 +163,9 @@ while running:
 
 	screen.fill(background_colour)
 
+	#draw ocean surface
+	pygame.draw.line(screen, [0,0,200], [0, 0.25*height], [width, 0.25*height], 1)
+
 	#draw patches
 	for p in patches:
 		if p.starter_patch:
@@ -146,7 +176,9 @@ while running:
 	pos = pygame.mouse.get_pos()
 	for p in patches:
 		if distance(p.pos, pos) < 50:
-			textsurface = myfont.render("Patch Type : " + str(p.patch_type) + "  Depth : " + str(p.depth) + "m", True, [255,255,255])
+			textsurface = myfont.render("Patch Type : " + str(p.patch_type) + 
+				"  Depth : " + str(p.depth) + "m" +
+				"  Sunlight : " + str(round(p.sunlight,2)) + "w/m^2", True, [255,255,255])
 			screen.blit(textsurface,(10, 10))
 
 
