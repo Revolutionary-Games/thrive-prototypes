af252afc653b017cbdc13bef6a7b345742fc0473
adQuid <28126232+adQuid@users.noreply.github.com>
2021-07-17 11:35:33 -0400

    init multicell_auto_evo solution

---
+++ multicell_auto_evo/MulticellAutoEvo/MulticellAutoEvo/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MulticellAutoEvo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SanityTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
